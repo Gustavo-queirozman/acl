a12dfac520d1a2b1368c55f29e0916016842ac24
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-08-28 10:23:58 -0300

    add auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Gate::define('manage_task', function(User $user) {
+
+            return $user->is_admin === 1;
+        });
     }
 }
